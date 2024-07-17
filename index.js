a294b7933ef2bdd467095f00611b1526a9129922
Levi Adissi <leviadissi@gmail.com>
2024-07-16 19:25:29 -0700

    initial file upload!

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function genPass(pwdChoice) {
+    let pwdVal = ""
+    for (let i = 0; i < 15; i++) {
+        let randVal = Math.floor(Math.random()*91)
+        pwdVal += characters[randVal]
+    }
+    return pwdVal
+}
+
+function generate() {
+    document.getElementById("pwdOne").textContent = genPass()
+    document.getElementById("pwdTwo").textContent = genPass()
+}
+
+function copyTextOne() {
+    console.log("hit func")
+    let pwdOne = document.getElementById("pwdOne").textContent
+    navigator.clipboard.writeText(pwdOne.value);
+    alert("Copied the text: " + pwdOne.value);
+}
+
+function copyTextTwo() {
+    console.log("hit func")
+    let pwdTwo = document.getElementById("pwdTwo").textContent
+    navigator.clipboard.writeText(pwdTwo.value);
+    alert("Copied the text: " + pwdTwo.value);
+}
